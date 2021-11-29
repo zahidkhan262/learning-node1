1f6ff47825abd8277f2b9fba2e72f45ed5f1fd19
zahidkhan262 <zahidkhan007860@gmail.com>
2021-11-29 17:25:50 +0530

    add npm

--- htmlweb.js
+++ htmlweb.js
@@ -6,9 +6,11 @@ let page = `
 <input type="text"  placeholder="type here..."/><br><br>
 <button>Submit</button>
 `
+var ucase = require('upper-case');
 
 http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(page)
+    res.write(ucase.upperCase("zahidkhan"))
     res.end();
 }).listen(4000);
