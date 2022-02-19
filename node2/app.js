1602c8bdaa1d11488586f689b9c0cd9a2c364ef0
ZAHID KHAN <76157268+zahidkhan262@users.noreply.github.com>
2022-02-19 18:22:45 +0530

    Update app.js

--- node2/app.js
+++ node2/app.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 const app = express();
 
-const port = 5000;
+// const port = 5000;
 
 app.get('/', (req, resp) => {
     resp.send("hello node js")
 })
-app.listen(port, () => {
+app.listen(4000, () => {
     console.log("server is loading")
 })
